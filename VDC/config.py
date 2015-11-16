783491bb84bf140edf30887edf43f32a4c8d9eac
visiondecristo <lmateo@visiondecristo.org>
2015-11-15 21:07:57 -0500

    Update config.py

--- VDC/config.py
+++ VDC/config.py
@@ -6,7 +6,7 @@ DEBUG = True
 
 NAME = 'LEO IS GOD CHILD'
 WTF_CSRF_ENABLED = True
-app.secret_key = 'x1c\x07O\xa3\xce\x80\x9eQ\x8f\xd19\xd3\xe8\xd3n|k\xe1\xc2\x10\xef\xe6\xc8,'
+app.secret_key = ''
 
 #Administrator
 ADMIN = 'cjusto777@gmail.com'
@@ -19,5 +19,5 @@ MAIL_PORT = 465
 MAIL_USE_TLS = False
 MAIL_USE_SSL = True
 MAIL_USERNAME = 'cjusto777@gmail.com'
-MAIL_PASSWORD = 'Saved777'
+MAIL_PASSWORD = ''
 
